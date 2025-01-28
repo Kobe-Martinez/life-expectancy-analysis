443da1b64e5b7195e84435ed3af507edc591e5c7
Kobe Martinez <kobe.martinez@myyahoo.com>
2025-01-28 00:43:34 -0500

    Update and rename Assignment4_Question3_KLM.R to life_expectancy_modeling.R

--- life_expectancy_modeling.R
+++ life_expectancy_modeling.R
@@ -1,7 +1,3 @@
-#21:219:220 Fundamentals of Data Visualization, Spring 2021
-#Assignment 4 - Question 3
-#Kobe Lee Martinez, RUID: 202006380
-
 library(GISTools)
 library(Cairo)
 library(RColorBrewer)
